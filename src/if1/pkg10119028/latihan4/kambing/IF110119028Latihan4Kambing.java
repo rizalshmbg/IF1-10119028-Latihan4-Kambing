e13e53cb4961b4125372cd0b073c854a58565fb4
Rizal Sihombing <66808677+rizalshmbg@users.noreply.github.com>
2020-10-18 01:07:24 +0700

    Add files via upload

---
+++ src/if1/pkg10119028/latihan4/kambing/IF110119028Latihan4Kambing.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119028.latihan4.kambing;
+
+/**
+ *
+ * @author
+ * NAMA     : Rizal Sihombing
+ * KELAS    : IF1
+ * NIM      : 10119028
+ * Deskripsi Program : Program ini berisi program untuk
+ */
+public class IF110119028Latihan4Kambing {
+
+    public void tambahKambing() {
+        // Deklarasi variabel lokal
+        int jumlahKambing = 0;
+        
+        jumlahKambing = jumlahKambing + 5;
+        System.out.println("Jumlah Kambing setelah ditambah: " + jumlahKambing);
+    }
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Kambing kambingJantan = new Kambing();
+        kambingJantan.tambahKambing();
+    }
+    
+}
---
+++ src/if1/pkg10119028/latihan4/kambing/Kambing.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119028.latihan4.kambing;
+
+/**
+ *
+ * @author RizalSihombing
+ */
+class Kambing {
+
+    void tambahKambing() {
+        // Deklarasi variabel lokal
+        int jumlahKambing = 0;
+        
+        jumlahKambing = jumlahKambing + 5;
+        System.out.println("Jumlah Kambing setelah ditambah: " + jumlahKambing);
+    }
+    
+}
